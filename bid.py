19c07176db36fb2786a5fc55333899aa1b1a9a78
Antek <antek.gawron@gmail.com>
2024-01-07 16:23:51 +0100

    add basic fold mechanics

--- bid.py
+++ bid.py
@@ -14,17 +14,37 @@ def displayPlayers(PLAYERSARRAY):
   for player in PLAYERSARRAY:
     print(f"{player['nick']} {player['role']}: {player['credits']}$")
 
+
 def makeMove(PLAYERSARRAY, playerOnMove, moves):
   print(f"Ruch gracza: {PLAYERSARRAY[playerOnMove]['nick']}")
   i=1
   for move in moves:
-    print(f"{move}")
+    print(f"{i}. {move}")
     i+=1
-  playersMove = int(input("Twój wybór: "))
+  playersMove=""
+  while not (playersMove==1 or playersMove==2 or playersMove==3):
+    playersMove = int(input("Twój wybór: "))
+    if not (playersMove==1 or playersMove==2 or playersMove==3):
+      print("Wybierz poprawną opcję")
+  return moves[playersMove-1]
+
+def fold(PLAYERSARRAY, playerIndex):
+  popPlayer = PLAYERSARRAY.pop(playerIndex)
+  return PLAYERSARRAY
+
+def continueBidding(PLAYERSARRAY, playerOnMove, possibleMoves):
+  decision=makeMove(PLAYERSARRAY, playerOnMove, possibleMoves)
+  if decision=="fold":
+    newPlayersArray = fold(PLAYERSARRAY, playerOnMove)
+    if len(newPlayersArray)==1:
+      print(f"Wygrywa gracz {newPlayersArray[0]['nick']}")
+      return 0
+    nextPlayer = setNextPlayer(playerOnMove-1, len(newPlayersArray))
+    continueBidding(newPlayersArray, nextPlayer, ['fold', 'call', 'raise'])
 
 
-
-def startBidding(PLAYERSARRAY):
+def startBidding(oryginalArray):
+  PLAYERSARRAY = oryginalArray.copy()
   playerOnMove=0
   for i in range(len(PLAYERSARRAY)):
     if PLAYERSARRAY[i]['role']=='SB':
@@ -36,6 +56,7 @@ def startBidding(PLAYERSARRAY):
   placeABet(PLAYERSARRAY, playerOnMove, 2*SMALLBLIND) ## big blind
   playerOnMove = setNextPlayer(playerOnMove, len(PLAYERSARRAY))
   displayPlayers(PLAYERSARRAY)
+  continueBidding(PLAYERSARRAY,playerOnMove, ['fold', 'call', 'raise'])
   
 
 
